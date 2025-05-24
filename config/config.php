3c1f4b93a3603d0513e072889aca33c25d102045
sanjar <38586745+sanjar8855@users.noreply.github.com>
2025-05-25 00:33:24 +0500

    '.'

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
 return [
     'db' => [
         'host' => '127.0.0.1',
-        'name' => 'energy',
-        'user' => 'root',
-        'pass' => '',
+        'name' => 'energiyasamarado',
+        'user' => 'energiyasamarado',
+        'pass' => 'd0956e3cec85a',
         'charset' => 'utf8mb4',
     ],
 ];
